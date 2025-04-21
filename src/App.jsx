309995bdbec633083154b91736e0e862f99f3921
Aparna Singha <aparna.24bcs10353@sst.scaler.com>
2025-04-21 13:07:29 +0000

    Refactor updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Layout from "./components/LayOut/Layout";
 import HomePage from "./pages/home/home_page";
 import AuthPage from "./pages/auth/auth_page";
 
 import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
-import BottomButton from "./components/BottomButton/BottomButton";
+import UserButton from "./components/UserButton/UserButton";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <BottomButton />
+      <UserButton />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
