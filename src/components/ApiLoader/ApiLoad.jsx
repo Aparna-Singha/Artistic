1699fae1dc0244d9dfecaac5f08d2f56d0f2f209
Aparna Singha <aparna.24bcs10353@sst.scaler.com>
2025-04-22 07:21:30 +0530

    Better Loading Screen

--- src/components/ApiLoader/ApiLoad.jsx
+++ src/components/ApiLoader/ApiLoad.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import "./ApiLoader.css";
 import { pingApi } from "../../api/ping";
+import "./ApiLoader.css";
 
 const ApiLoader = () => {
   const [loading, setLoading] = useState(true);
+  const [longerLoading, setLongerLoading] = useState(false);
 
   useEffect(() => {
     const ping = async () => {
@@ -14,6 +15,16 @@ const ApiLoader = () => {
     ping();
   }, []);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (loading) {
+        setLongerLoading(true);
+      }
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [loading]);
+
   if (!loading) return null;
 
   return (
@@ -21,7 +32,15 @@ const ApiLoader = () => {
       <div className="loader-content">
         <span className="loader-spinner" />
         <span className="loader-text">
-          Connecting to the server...
+          <span className="normal-loading">
+            Connecting to the server...
+          </span>
+          {longerLoading &&
+            <span className="longer-loading">
+              The server is waking up from a nap...
+              This may take upto a minute...
+            </span>
+          }
         </span>
       </div>
     </div>
